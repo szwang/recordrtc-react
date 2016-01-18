71aaa5e72069e2ee5c9079c858f308c9996e30af
Suzanne Wang <suzanneswang@gmail.com>
2016-01-18 12:12:00 -0500

    [feat] hook up App with main page

---
+++ src/js/App.react.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+import RecordPage from './components/RecordPage.react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <RecordPage />
+      </div>
+    )
+  }
+}
+
+export default App;
