d0a8f5dac54140bbff2b16eba0997e73d57748b5
Suzanne Wang <suzanneswang@gmail.com>
2016-01-18 09:16:04 -0500

    [chore] add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/app.js'
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './build')
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel?presets[]=react,presets[]=es2015,presets[]=stage-0']
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
